e5715fe3348bf3d0f170bcb656760a897d93fa0a
Tengku Asra <tengku1900016197@webmail.uad.ac.id>
2022-01-23 15:36:19 +0700

    Initial commit

---
+++ application/views/layout/team.php
@@ -0,0 +1,23 @@
+        <!-- Team-->
+        <section class="page-section bg-light" id="team">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Owner</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="<?php echo base_url()?>assets/img/team/tengku asra.jpg" alt="" />
+                            <h4>Tengku Asraa Fariza</h4>
+                            <p class="text-muted">1900016197</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted"></p></div>
+                </div>
+            </div>
---
+++ application/views/layout/tentang.php
@@ -0,0 +1,24 @@
+<!-- About-->
+        <section class="page-section" id="about">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Tentang Aplikasi</h2>
+                    <h3 class="section-subheading text-muted">Aplikasi ini dibuat untuk memenuhi tugas mata kuliah Rekayasa Perangkat Lunak.</h3>
+                </div>
+                <ul class="timeline">
+                    <li>
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url()?>assets/img/about/4.jpg" alt="" /></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4 class="subheading">AstegShop</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted"><i> AstegShop adalah sebuah toko kelontong yang berada di daerah Sambas tepatnya di Kalimantan Barat, AstegShop didirikan pada tanggal 9 april 2002. AstegShop dibangun untuk memenuhi kebutuhan masyarakat sekitar, bangunan ini tepat berdiri di depan Sekolah Dasar Negeri Sambas. </p></div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
